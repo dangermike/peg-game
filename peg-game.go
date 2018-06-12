49938f9308f80950281991d6200072a1cd83ae68
Mike Hurwitz <dangermike@gmail.com>
2018-06-11 22:42:50 -0400

    bitwise. for efficency

--- peg-game.go
+++ peg-game.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/bits"
 	"runtime"
 	"sync"
 )
@@ -19,7 +20,7 @@ type Move struct {
 	to   byte
 }
 
-var moves = [...]Move{
+var moves = []Move{
 	Move{0, 1, 3},
 	Move{1, 3, 6},
 	Move{3, 6, 10},
@@ -63,28 +64,41 @@ var moves = [...]Move{
 	Move{3, 4, 5},
 }
 
+type BitMove struct {
+	from uint16
+	to   uint16
+}
+
+func BitMoveFromMove(move Move) BitMove {
+	return BitMove{
+		uint16(1<<move.from) | uint16(1<<move.over),
+		1 << move.to,
+	}
+}
+
+func BitMoves(moves []Move) []BitMove {
+	ret := make([]BitMove, len(moves), len(moves))
+	for i := 0; i < len(moves); i++ {
+		ret[i] = BitMoveFromMove(moves[i])
+	}
+	return ret
+}
+
+var bitMoves = BitMoves(moves)
+
 // Board holds pegs, indicated by their bool value
-type Board [15]bool
+type Board uint16
 
 // NewBoard makes a board with one peg open
 func NewBoard() *Board {
 	var b Board
-	for i := 0; i < len(b); i++ {
-		b[i] = true
-	}
-	b[4] = false
+	b = ((1 << 15) - 1) ^ (1 << 4)
 	return &b
 }
 
 // PegCount counts pegs
 func (b *Board) PegCount() int {
-	cnt := 0
-	for i := 0; i < len(b); i++ {
-		if b[i] {
-			cnt++
-		}
-	}
-	return cnt
+	return bits.OnesCount16(uint16(*b))
 }
 
 // IsComplete means the board only has one peg
@@ -92,8 +106,8 @@ func (b *Board) IsComplete() bool {
 	return 1 >= b.PegCount()
 }
 
-func (b *Board) peg(ix int) string {
-	if b[ix] {
+func (b *Board) peg(ix uint) string {
+	if (uint16(*b) & (uint16(1) << ix)) > 0 {
 		return "*"
 	}
 	return "O"
@@ -110,14 +124,7 @@ func (b *Board) Print() {
 
 // ToNumber Generates a unique number for each board
 func (b *Board) ToNumber() int {
-	r := 0
-	for i := len(b) - 1; i >= 0; i-- {
-		r <<= 1
-		if b[i] {
-			r++
-		}
-	}
-	return r
+	return int(*b)
 }
 
 // BoardMove is a tuple of a board and a move
@@ -131,7 +138,7 @@ func (bm *BoardMove) printMovesInner() {
 	if bm.prev != nil {
 		bm.prev.printMovesInner()
 	}
-	if bm.move.from >= 0 {
+	if bm.move.from < 255 {
 		fmt.Print(bm.move)
 	}
 }
@@ -151,14 +158,12 @@ var empty = Empty{}
 func NextStates(baseMove *BoardMove, toCheck chan<- *BoardMove) int {
 	b := baseMove.board
 	c := 0
+	bval := uint16(*b)
 	for i := 0; i < len(moves); i++ {
-		move := moves[i]
-		if (b[move.from]) && (b[move.over]) && (!b[move.to]) {
-			n := *b
-			n[move.from] = false
-			n[move.over] = false
-			n[move.to] = true
-			toCheck <- &BoardMove{&n, move, baseMove}
+		move := bitMoves[i]
+		if (bval&move.from == move.from) && (bval&move.to == 0) {
+			nb := Board((bval ^ move.from) | move.to)
+			toCheck <- &BoardMove{&nb, moves[i], baseMove}
 			c++
 		}
 	}
@@ -166,21 +171,25 @@ func NextStates(baseMove *BoardMove, toCheck chan<- *BoardMove) int {
 }
 
 func main() {
+	workerCnt := runtime.NumCPU() - 1
+	if workerCnt == 0 {
+		workerCnt = 1
+	}
 	toCheck := make(chan *BoardMove, 1)
 	toExpand := make(chan *BoardMove, 200*runtime.NumCPU())
 	c := 0
 	var wg sync.WaitGroup
-	for x := 0; x < runtime.NumCPU(); x++ {
+	for x := 0; x < workerCnt; x++ {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
-			toCheck <- &BoardMove{NewBoard(), Move{0, 0, 0}, nil}
 			for ps := range toExpand {
 				NextStates(ps, toCheck)
 			}
 		}()
 	}
 
+	toCheck <- &BoardMove{NewBoard(), Move{255, 255, 255}, nil}
 	seenBoards := make(map[int]struct{})
 	for bs := range toCheck {
 		pc := bs.board.PegCount()
@@ -201,6 +210,9 @@ func main() {
 			}
 		}
 	}
+
+	// none of this is necessary, but this is the way to gracefully shut down.
+	// in case someone wants to use this as a library. hahahahaha
 	close(toExpand)
 	go func() {
 		wg.Wait()
